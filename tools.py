a9cf80b833bfd59b92d6949966d86b2756e76789
Noor Abdullah <sheikhnoorabdullah02@gmail.com>
2026-06-19 12:36:30 +0600

    Tavily COnnect to project

---
+++ tools.py
@@ -0,0 +1,35 @@
+from langchain.tools import tool
+import requests
+from bs4 import BeautifulSoup
+
+from tavily import TavilyClient
+
+from rich import print
+
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+
+tavily = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
+
+
+@tool
+def web_search(query: str) ->str:
+    """
+    Search the web for a given query and return the top results.
+    """
+    results = tavily.search(query=query,max_results=5)
+
+    out = []
+
+    for r in results['results']:
+        out.append(
+            f"Title: {r['title']}\nURL: {r['url']}\nSnippet: {r['content'][:300]}\n"
+        )
+
+    return "\n------\n".join(out)
+
+print(web_search.invoke("What is the capital of Bangladesh?"))
